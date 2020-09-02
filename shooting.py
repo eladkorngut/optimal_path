572f1f97fb99cc911f5d3d87262de4eb2750d77a
elad <elad.korngut@mail.huji.ac.il>
2020-09-02 14:52:50 +0300

    A working one dimensional program for the shooting method. The program works well for a sis model, and reproduce the path. It need to be cleaner in terms that it need a way to store the paths and plot them. It also has a csv functionality that might not be usefull

--- shooting.py
+++ shooting.py
@@ -4,9 +4,22 @@ import matplotlib
 from scipy.optimize import least_squares
 from scipy.integrate import ode
 import scipy.linalg as la
+from scipy.integrate import odeint
+from pylab import figure, plot, xlabel, grid, legend, title,savefig
+from matplotlib.font_manager import FontProperties
+import csv
 
 
 ecl_dist=lambda xi,yi,xf,yf: np.sqrt((xi-xf)**2+(yi-yf)**2)
+dx_dt = lambda i,p,b,k: b*(1-i)*i*np.exp(p)-k*i*np.exp(-1*p)
+dp_dt = lambda i,p,b,k: b*(2*i-1)*(np.exp(p)-1)-k*(np.exp(-1*p)-1)
+
+def vectorfiled(q,t,m):
+    x,p = q
+    b,k = m
+    f=[dx_dt(x,p,b,k),dp_dt(x,p,b,k)]
+    return f
+
 
 def shoot(x0,p0,dx_dt,dp_dt,dt,pf,xf):
     x_path,p_path = [x0], [p0]
@@ -23,14 +36,35 @@ def shoot(x0,p0,dx_dt,dp_dt,dt,pf,xf):
     resdiual = np.min([ecl_dist(i,j,xf,pf) for i, j in zip(x_path, p_path)])
     return (x_path,p_path),resdiual
 
+def shoot_ode(x0,p0,b,k):
+    # ODE parameters
+    abserr = 1.0e-8
+    relerr = 1.0e-6
+    stoptime = 10.0
+    numpoints = 250
+
+    # create the time samples for the output of the ODE solver
+    t = [stoptime * float(i) / (numpoints - 1) for i in range(numpoints)]
+    q0 = (x0,p0)
+    m0 = (b,k)
+    qsol = odeint(vectorfiled,q0,t,args=(m0,),atol=abserr,rtol=relerr)
+
+    f=open('temp_print_numbers.csv','w+')
+    with f:
+        writer =csv.writer(f)
+        writer.writerows(qsol)
+
+    return qsol
+
+
+
+# The begining of the original program
 paths,residual=[],[]
 k,b,r,dt=1.0,5,0.1,0.001
 theta=np.linspace(np.pi/100,np.pi,20)
 x0,p0,xf,pf =1-k/b,0,0,np.log(k/b)
 xi = [x0-r*np.cos(t) for t in theta]
 pi = [p0 -r*np.sin(t) for t in theta]
-dx_dt = lambda i,p: b*(1-i)*i*np.exp(p)-k*i*np.exp(-1*p)
-dp_dt = lambda i,p: b*(2*i-1)*(np.exp(p)-1)-k*(np.exp(-1*p)-1)
 for x,p in zip(xi,pi):
     J = np.array([[(b*np.exp(p)-k*np.exp(-p))-2*b*x*np.exp(0),b*(1-x)*np.exp(p)+k*x*np.exp(-p)],[2*b*np.exp(p),b*(2*x-1)*np.exp(p)+k*np.exp(-p)]])
     eigen_value,eigen_vec=la.eig(J)
@@ -39,9 +73,10 @@ for x,p in zip(xi,pi):
     else:
         eig_value, eig_vec = eigen_value[1].real , eigen_vec[:,1].reshape(2,1)
     # eig_value,direction_vec = J_diag[0].real[0] , J_diag[1][0] if J_diag[0].real[0]<0 else J_diag[0].real[1] , J_diag[1][1]
-    path_current,residual_current=shoot(x0+float(eig_vec[0])*dt,p0+float(eig_vec[1])*dt,dx_dt,dp_dt,dt,pf,xf)
-    paths.append(path_current)
-    residual.append(residual_current)
-print('Lowest value',np.min(residual))
-print('choosen path',np.where(residual==np.min(residual)))
-print('this no love song')
+    # path_current,residual_current=shoot(x0+float(eig_vec[0])*dt,p0+float(eig_vec[1])*dt,dx_dt,dp_dt,dt,pf,xf)
+    solution=shoot_ode(x0+float(eig_vec[0])*dt,p0+float(eig_vec[1])*dt,b,k)
+    # paths.append(path_current)
+    # residual.append(residual_current)
+# print('Lowest value',np.min(residual))
+# print('choosen path',np.where(residual==np.min(residual)))
+# print('this no love song')
